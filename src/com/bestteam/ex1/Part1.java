abfbc47e55e71c0a66cf0f7117814c4fb9bc0c92
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-01-23 17:03:40 +0000

    Part 1

--- src/com/bestteam/ex1/Part1.java
+++ src/com/bestteam/ex1/Part1.java
@@ -1,20 +1,21 @@
 package com.bestteam.ex1;
 
 import lejos.nxt.Button;
-import rp.systems.RobotProgrammingDemo;
+import lejos.nxt.LCD;
 
-public class Part1 extends RobotProgrammingDemo {
+public class Part1 implements Runnable {
 	
 	public static void main(String[] args) {
+		Part1 pt1 = new Part1();
+		new Thread(pt1).start();
 	}
 
 	@Override
 	public void run() {
 		
 		Button.waitForAnyPress();
-		
-		while(m_run) {
-		}
+		System.out.println("Hello world");
+		Button.waitForAnyPress();
 		
 	}
 
