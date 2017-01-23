3e01237b510afe62e7f411524167c4dac34b36a6
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-01-23 16:30:09 +0000

    Package commit

---
+++ src/com/bestteam/ex1/Part1.java
@@ -0,0 +1,21 @@
+package com.bestteam.ex1;
+
+import lejos.nxt.Button;
+import rp.systems.RobotProgrammingDemo;
+
+public class Part1 extends RobotProgrammingDemo {
+	
+	public static void main(String[] args) {
+	}
+
+	@Override
+	public void run() {
+		
+		Button.waitForAnyPress();
+		
+		while(m_run) {
+		}
+		
+	}
+
+}
